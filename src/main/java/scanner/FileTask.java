89d4c38d58b720fd2c38cc93b26c73aea830146f
Ayesha <ayesha19765@gmail.com>
2026-07-31 20:16:10 +0530

    refactor codebase and finalize project documentation

--- src/main/java/scanner/FileTask.java
+++ src/main/java/scanner/FileTask.java
@@ -2,25 +2,55 @@ package scanner;
 
 import java.nio.file.Path;
 
+/**
+ * Represents a file waiting to be processed
+ * by backup workers.
+ *
+ * Immutable object passed through the
+ * producer-consumer queue.
+ */
 public class FileTask {
 
     private final Path filePath;
     private final long size;
 
+    /*
+     * Sentinel object used to notify workers
+     * that scanning is complete.
+     */
     public static final FileTask POISON_PILL =
             new FileTask(null, -1);
 
-    public FileTask(Path filePath, long size) {
+    public FileTask(
+            Path filePath,
+            long size
+    ) {
+
         this.filePath = filePath;
         this.size = size;
+
     }
 
     public Path getFilePath() {
+
         return filePath;
+
     }
 
     public long getSize() {
+
         return size;
+
+    }
+
+    @Override
+    public String toString() {
+
+        return "FileTask{" +
+                "filePath=" + filePath +
+                ", size=" + size +
+                '}';
+
     }
 
 }
